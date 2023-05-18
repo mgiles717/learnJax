074fd67eb8164139ef91d0fee6ecd01ad53d7941
mgiles717 <mgiles717@gmail.com>
2023-05-18 01:35:34 +0100

    Added several file templates

--- learnJax/optimizers/sgd.py
+++ learnJax/optimizers/sgd.py
@@ -13,6 +13,7 @@ class SGD(Optimizer):
         super().__init__(params, lr)
         
     def step(self):
+        # This method won't work, this is being built on JAX and these are torch parameters
         loss = None
         for param in self.params:
             if param.grad is not None:
